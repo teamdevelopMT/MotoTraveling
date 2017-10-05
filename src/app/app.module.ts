410145d6fcffee09b1a7dcbd84391ebb12be0e77
Jerarck <edison-rojas@hotmail.es>
2017-10-05 00:48:29 -0500

    Se agrega como page root el login para inicio de aplicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {LoginPage} from '../pages/login/login'
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp, 
+    LoginPage
   ],
   imports: [
     BrowserModule,
@@ -18,8 +18,8 @@ import { HomePage } from '../pages/home/home';
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp, 
+    LoginPage
   ],
   providers: [
     StatusBar,
